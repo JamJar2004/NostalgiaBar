95ecd69551eb03272e4a96be54136998753afc68
JamJar2004 <cutajarjames2004@gmail.com>
2024-07-25 13:39:23 +0200

    Initial Commit

---
+++ NostalgiaBar/Taskbar.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+
+namespace NostalgiaBar
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class Taskbar : Window
+    {
+        public Taskbar()
+        {
+            InitializeComponent();
+        }
+
+        private void Taskbar_Loaded(object sender, RoutedEventArgs e)
+        {
+            var screenWidth  = SystemParameters.PrimaryScreenWidth;
+            var screenHeight = SystemParameters.PrimaryScreenHeight;
+
+            Left  = 0;
+            Top   = screenHeight - Height;
+
+            Width = screenWidth;    
+        }
+    }
+}
